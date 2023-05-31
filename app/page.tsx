3b7c41687c22530aa4a1499c65d5c03bddb3bed2
arian <kapqworks@gmail.com>
2023-05-31 19:36:46 +0330

    BUGFIX Post.tsx promise<element> error

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import { createServerComponentSupabaseClient } from '@supabase/auth-helpers-nextjs'
 import { headers, cookies } from 'next/headers'
-import { Post } from '@/components/Post'
+import Post from '@/components/Post'
 
 // do not cache this page
 export const revalidate = 0
@@ -13,11 +13,10 @@ export default async function ServerComponent() {
   const { data } = await supabase.from('posts').select('*')
 
   return (
-    <div className="max-w-[468px] mx-auto grid grid-cols-1 text-center divide-y-[1px] divide-white divide-opacity-40 ">
+    <div className="max-w-[468px] mx-auto grid grid-cols-1 text-center divide-y-[1px] divide-white divide-opacity-40">
       {data?.map((post, i) => {
         return (
           <>
-            {/* @ts-expect-error Server Component */}
             <Post
               key={i}
               id={post.id}
--- components/Post.tsx
+++ components/Post.tsx
@@ -13,7 +13,7 @@ interface Post {
     image_path: string,
 }
 
-export const Post = async ({ id, creator_id, title, description, date, image_path }: Post) => {
+const Post = async ({ id, creator_id, title, description, date, image_path }: Post) => {
 
     const supabase = createServerComponentSupabaseClient({
         headers,
@@ -23,7 +23,7 @@ export const Post = async ({ id, creator_id, title, description, date, image_pat
     const { data: _imageUrl } = await supabase.storage.from('posts').getPublicUrl(image_path)
     const { data: _username } = await supabase.from('users').select('user_name').eq('user_id', creator_id)
 
-    const username = _username![0].user_name
+    const username = _username && _username[0].user_name
 
     return (
         <article
@@ -76,6 +76,7 @@ export const Post = async ({ id, creator_id, title, description, date, image_pat
                     src={_imageUrl.publicUrl}
                     width={1000}
                     height={1000}
+                    priority
                     style={{ objectFit: 'contain', width: '100%', height: 'auto' }}
                 />
             </div>
@@ -86,15 +87,15 @@ export const Post = async ({ id, creator_id, title, description, date, image_pat
                 <div className="flex gap-6">
                     {/* Like */}
                     <div className="flex items-center gap-2">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-8 h-8">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-8 h-8">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
                         </svg>
                     </div >
 
                     {/* Comment */}
                     < div className="flex items-center gap-2">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-8 h-8">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M12 20.25c4.97 0 9-3.694 9-8.25s-4.03-8.25-9-8.25S3 7.444 3 12c0 2.104.859 4.023 2.273 5.48.432.447.74 1.04.586 1.641a4.483 4.483 0 01-.923 1.785A5.969 5.969 0 006 21c1.282 0 2.47-.402 3.445-1.087.81.22 1.668.337 2.555.337z" />
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-8 h-8">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M12 20.25c4.97 0 9-3.694 9-8.25s-4.03-8.25-9-8.25S3 7.444 3 12c0 2.104.859 4.023 2.273 5.48.432.447.74 1.04.586 1.641a4.483 4.483 0 01-.923 1.785A5.969 5.969 0 006 21c1.282 0 2.47-.402 3.445-1.087.81.22 1.668.337 2.555.337z" />
                         </svg>
                     </div >
                 </div>
@@ -123,3 +124,5 @@ export const Post = async ({ id, creator_id, title, description, date, image_pat
         </article >
     )
 }
+
+export default Post as any
