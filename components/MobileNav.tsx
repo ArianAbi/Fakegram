a92218801c309df99b3e718502f9cf61b311c200
arian <kapqworks@gmail.com>
2023-06-15 13:47:49 +0330

    Update : MobileNav buttons take the whole space

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -20,7 +20,9 @@ const MobileNav = async () => {
                 {/* home */}
                 <div className="w-full h-full flex items-center justify-center">
 
-                    <Link href="/">
+                    <Link
+                        className="w-full h-full flex items-center justify-center"
+                        href="/">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor"
                             className="w-7 h-7">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
@@ -33,6 +35,7 @@ const MobileNav = async () => {
                 <div className="w-full h-full flex items-center justify-center">
 
                     <Link
+                        className="w-full h-full flex items-center justify-center"
                         href={
                             user === null || undefined ? '/login'
                                 : `/${_username && _username[0].user_name}/create`
@@ -50,6 +53,7 @@ const MobileNav = async () => {
                 <div className="w-full h-full flex items-center justify-center">
 
                     <Link
+                        className="w-full h-full flex items-center justify-center"
                         href={
                             user === null || undefined ? '/login'
                                 : `/${_username && _username[0].user_name}`
