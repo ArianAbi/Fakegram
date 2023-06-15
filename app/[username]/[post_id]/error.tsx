c08c55bcf31da4678c6732b4c09ba91cf6f3dcc2
arian <kapqworks@gmail.com>
2023-06-15 13:55:54 +0330

    Add : Error Page for dynamic Post Page

---
+++ app/[username]/[post_id]/error.tsx
@@ -0,0 +1,30 @@
+'use client'
+
+import { Button } from "@/components/ui/button";
+
+export default function Error({
+    error,
+    reset,
+}: {
+    error: Error
+    reset: () => void
+}) {
+
+    console.log(error);
+
+    return (
+        <>
+            <div className="flex flex-col items-center justify-center">
+                <h2 className="text-2xl mt-8">Something Went Wrong {':('}</h2>
+                <span className="italic mt-4">{error.message}</span>
+
+                <Button
+                    className="mt-4 bg-red-500"
+                    onClick={() => reset()}
+                >
+                    Try Again
+                </Button>
+            </div>
+        </>
+    )
+}
