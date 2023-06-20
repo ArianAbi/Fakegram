5c28faf2359227c5693e9af8ca6a51ca6cff742e
arian <kapqworks@gmail.com>
2023-06-20 17:36:17 +0330

    Update : use ReactShowMoreText for <Description /> truncate

--- components/Description.tsx
+++ components/Description.tsx
@@ -2,6 +2,7 @@
 
 import Link from "next/link"
 import { useEffect, useRef, useState } from "react"
+import ReactShowMoreText from "react-show-more-text"
 
 interface Description {
     author_id: string,
@@ -11,48 +12,21 @@ interface Description {
 
 const Description = ({ author_id, author_username, description }: Description) => {
 
-    const [showMore, setShowMore] = useState(false)
-
-    const [isMoreThanThreeLines, setIsMoreThanThreeLines] = useState(false);
-    const hiddenSpanRef = useRef<HTMLSpanElement>(null);
-
-    useEffect(() => {
-        // Assumption: Line height is 20px. Please adjust as per your CSS
-        if (hiddenSpanRef.current?.offsetHeight! > 80) {
-            setIsMoreThanThreeLines(true);
-        } else {
-            setIsMoreThanThreeLines(false);
-        }
-    }, [description]);
-
-
     return (
-        <div
-            className={`relative w-full text-left text-[0.95rem] text-white mb-4 px-4 ${!showMore ? "line-clamp-3" : ""}`}
-        >
-            <Link
-                href={`/${author_username}`}
-                className="pr-2 font-semibold text-base inline"
+        <div className="w-full mb-4 px-4">
+            <ReactShowMoreText
+                expanded={false}
+                className="w-full text-left"
             >
-                {author_username}
-            </Link>
+                <Link
+                    href={`/${author_username}`}
+                    className="pr-2 font-semibold text-base inline"
+                >
+                    {author_username}
+                </Link>
 
-            <span
-                ref={hiddenSpanRef}
-            >
                 {description}
-            </span>
-
-            {isMoreThanThreeLines &&
-                <div className="absolute backdrop-blur-3xl bottom-0 right-7 pl-4">
-                    <input
-                        className={showMore ? "appearance-none after:content-['show_less']" : "appearance-none after:content-['._._._show_more']"}
-                        type="checkbox"
-                        checked={showMore}
-                        onClick={() => setShowMore(!showMore)}
-                    />
-                </div>
-            }
+            </ReactShowMoreText>
         </div>
     )
 }
