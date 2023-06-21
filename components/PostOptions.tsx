c9db04f31f8db67802daa10bc7622ad711629b1b
arian <kapqworks@gmail.com>
2023-06-21 15:41:54 +0330

    Add : onTap for postOptions buttons with framerMotion

--- components/PostOptions.tsx
+++ components/PostOptions.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { useState } from "react"
+import { motion } from "framer-motion"
 
 export default function PostOptions() {
 
@@ -24,25 +25,38 @@ export default function PostOptions() {
 
                 </button>
 
-                <div className={`absolute text-sm flex flex-col items-center justify-center divide-y-[1px] backdrop-blur-md right-4 top-4 w-[160px] p-2 bg-transparent rounded-md border-[1px] border-white border-opacity-25 z-20
+                <div
+                    className={`absolute text-sm flex flex-col items-center justify-center divide-y-[1px] backdrop-blur-md right-4 top-4 w-[160px] py-2 px-4 bg-transparent rounded-md border-[1px] border-white border-opacity-25 z-20
                 origin-top-right ${open ? 'scale-100' : 'scale-0'} transition-all duration-75`}>
-                    <button
-                        className="bg-transparent active:bg-black active:bg-opacity-50 h-[40px] w-full"
+                    <motion.button
+                        whileTap={{ scale: 0.9 }}
+                        className="flex items-center justify-between bg-transparent h-[40px] w-full"
                         onClick={() => {
                             console.log('copy post url');
                         }}
                     >
                         Copy Post URL
-                    </button>
 
-                    <button
-                        className="bg-transparent active:bg-black active:bg-opacity-50 h-[40px] w-[90%]"
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M15.666 3.888A2.25 2.25 0 0013.5 2.25h-3c-1.03 0-1.9.693-2.166 1.638m7.332 0c.055.194.084.4.084.612v0a.75.75 0 01-.75.75H9a.75.75 0 01-.75-.75v0c0-.212.03-.418.084-.612m7.332 0c.646.049 1.288.11 1.927.184 1.1.128 1.907 1.077 1.907 2.185V19.5a2.25 2.25 0 01-2.25 2.25H6.75A2.25 2.25 0 014.5 19.5V6.257c0-1.108.806-2.057 1.907-2.185a48.208 48.208 0 011.927-.184" />
+                        </svg>
+
+                    </motion.button>
+
+                    <motion.button
+                        whileTap={{ scale: 0.9 }}
+                        className="flex items-center justify-between bg-transparent h-[40px] w-full"
                         onClick={() => {
-                            console.log('copy post url');
+                            console.log('delete post');
                         }}
                     >
                         Delete Post
-                    </button>
+
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+                        </svg>
+
+                    </motion.button>
 
                 </div>
             </div>
