f43ea793d5e018775f49674618c6e04e93e1359b
arian <kapqworks@gmail.com>
2023-07-03 15:42:27 +0330

    Remove : plus icon from CreatedToast in `ToastComponents`

--- components/ToastComponents.tsx
+++ components/ToastComponents.tsx
@@ -27,14 +27,6 @@ export function CreatedToast() {
     return (
         <div className="flex gap-2 w-full justify-center">
             Post Created !
-
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 stroke-green-500">
-                <motion.path
-                    initial={{ rotateZ: 0 }}
-                    animate={{ rotateZ: 180 }}
-                    transition={{ duration: 0.25 }}
-                    strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
-            </svg>
         </div>
     )
 }
