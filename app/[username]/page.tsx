3896f2d34e81b5fb3931320f864e758d03a45a89
arian <kapqworks@gmail.com>
2023-07-04 14:28:46 +0330

    Update : `user profile` made respomsive `[username]`

--- app/[username]/page.tsx
+++ app/[username]/page.tsx
@@ -2,6 +2,7 @@ import { createServerComponentSupabaseClient } from "@supabase/auth-helpers-next
 import { headers, cookies } from "next/headers"
 import Image from "next/image"
 import ProfilePost from "./ProfilePost"
+import ProfileButtons from "./ProfileButtons"
 
 export default async function userPage({ params: { username } }: any) {
 
@@ -10,30 +11,64 @@ export default async function userPage({ params: { username } }: any) {
         cookies,
     })
 
-
     const { data: [{ user_id }] }: any = await supabase.from('users').select('*').eq('user_name', username)
     const { data: posts } = await supabase.from('posts').select('*').eq('creator_id', user_id)
 
     return (
-        <section>
+        <section className="max-w-[924px] w-full mx-auto pt-4">
             {/* profile */}
-            <div className="flex gap-4 p-4 text-xl">
-                <Image
-                    className="rounded-full"
-                    src={'/defaultProfile.png'}
-                    width={70}
-                    height={70}
-                    priority
-                    alt="profile"
-                />
+            <div className="flex justify-start md:justify-between md:grid md:grid-cols-3 grid-rows-1 items-start gap-4 p-4 text-2xl">
+                {/* profile image */}
+                <div className="md:col-span-1 md:justify-self-end md:pr-8">
+                    {/* mobile profile image */}
+                    <Image
+                        className="md:hidden rounded-full inline"
+                        src={'/defaultProfile.png'}
+                        width={70}
+                        height={70}
+                        priority
+                        alt="profile"
+                    />
 
-                <h2>{username}</h2>
+                    {/* desktop profile image */}
+                    <Image
+                        className="hidden md:inline rounded-full"
+                        src="/defaultProfile.png"
+                        width={130}
+                        height={130}
+                        priority
+                        alt="profile"
+                    />
+                </div>
+
+                {/* edit profile button */}
+                <div className="col-span-1 flex flex-col items-center justify-center gap-4 pl-4 md:pl-0">
+
+                    <div className="flex flex-col md:flex-row justify-start w-full gap-3 md:gap-6">
+                        <h2>{username}</h2>
+
+                        <ProfileButtons username={username} />
+                    </div>
+
+                    {/* post count for large screens */}
+                    <span className="hidden md:inline w-full text-base">{posts?.length} posts</span>
+
+                    {/* bio for large screens */}
+                    <p className="hidden md:block text-sm">
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dicta alias aliquam asperiores corporis placeat iste autem numquam reiciendis inventore temporibus?
+                    </p>
+                </div>
             </div>
 
-            {/* content */}
-            <div className="flex flex-col items-center w-full text-base py-4">
-                <span>{posts?.length}</span>
-                <span className="text-gray-400">post</span>
+            {/* bio for smaller screens */}
+            <p className="md:hidden text-sm px-4 mt-4 mb-8">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dicta alias aliquam asperiores corporis placeat iste autem numquam reiciendis inventore temporibus?
+            </p>
+
+
+            {/* Post `with the count on smaller screens` */}
+            <div className="w-full border-t-[1px] border-stone-600 text-center text-base py-4 md:text-lg">
+                <span className="md:hidden">{posts?.length}</span> Posts
             </div>
 
             <div className="grid grid-cols-3 gap-1">
