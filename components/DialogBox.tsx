c087eff4f6f97290b8a6d8a9feb7af6c17e89361
arian <kapqworks@gmail.com>
2023-07-09 11:35:09 +0330

    Update : `DialogBox` animate on opening

--- components/DialogBox.tsx
+++ components/DialogBox.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+import { motion } from 'framer-motion'
+
 interface DialogBox {
     title: string,
     discard: string,
@@ -14,14 +16,19 @@ export default function DialogBox({ title, agree, discard, onAgree, onDiscard, l
     return (
         <>
             {/* backdrop */}
-            <div
+            <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
                 className="fixed flex justify-center items-center bg-black bg-opacity-50 backdrop-blur-sm w-full h-full z-40"
                 onClick={() => onDiscard()}
             >
-            </div>
+            </motion.div>
 
             {/* dialogBox */}
-            <div className="absolute top-[50%] right-[50%] translate-x-[50%] translate-y-[-50%] flex flex-col items-center justify-center gap-6 p-8 bg-black bg-opacity-70 border-[1px] border-stone-600 text-white rounded-md z-[9999]">
+            <motion.div
+                initial={{ scale: 0.7, translateX: "50%", translateY: "-50%" }}
+                animate={{ scale: 1, translateX: "50%", translateY: "-50%" }}
+                className="fixed top-[50%] right-[50%] translate-x-[50%] translate-y-[-50%] flex flex-col items-center justify-center gap-6 p-8 bg-black bg-opacity-70 border-[1px] border-stone-600 text-white rounded-md z-[9999]">
                 <h2 className="text-lg font-semibold">
                     {title}
                 </h2>
@@ -43,7 +50,7 @@ export default function DialogBox({ title, agree, discard, onAgree, onDiscard, l
                         {loadingState && loadingState ? loadingMessage : agree}
                     </button>
                 </div>
-            </div>
+            </motion.div>
         </>
     )
 }
