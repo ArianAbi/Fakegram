6f68152da1cb23f22229d8d2fe90a7418e82f60d
arian <kapqworks@gmail.com>
2023-07-09 11:59:08 +0330

    Update : `Profile page` show no post message if user has no post

--- app/[username]/page.tsx
+++ app/[username]/page.tsx
@@ -72,6 +72,19 @@ export default async function userPage({ params: { username } }: any) {
             </div>
 
             <div className="grid grid-cols-3 gap-1">
+                {!posts || posts.length === 0 &&
+                    <div className="col-span-full w-full flex flex-col items-center justify-center gap-2 mt-4">
+                        {/* photo svg */}
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.2} stroke=""
+                            className="w-28 h-28 lg:w-32 lg:h-32 stroke-stone-500">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
+                        </svg>
+
+                        {/* no post text */}
+                        <h2 className="text-3xl lg:text-4xl text-stone-500 italic font-semibold">No Posts!</h2>
+                    </div>
+                }
+
                 {posts?.map(post => <>
                     <ProfilePost
                         key={post.id}
